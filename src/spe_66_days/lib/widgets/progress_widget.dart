f32b5636dd9b1eb204ef91bafae95a475edd7dc4
Jacob Chalk <jacob_chalkie_@hotmail.co.uk>
2018-12-10 22:23:08 +0000

    Attempted formatting x axis on library, half completed

--- src/spe_66_days/lib/widgets/progress_widget.dart
+++ src/spe_66_days/lib/widgets/progress_widget.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:spe_66_days/classes/HabitManager.dart';
-/*This class will display some sample lineGraph
-*/
+
 class ProgressWidget extends StatefulWidget implements BottomNavigationBarItem {
   final Icon icon;
   final Text title;
@@ -19,16 +18,26 @@ class ProgressWidget extends StatefulWidget implements BottomNavigationBarItem {
 
 
 class _ProgressState extends State<ProgressWidget>{
+  DateTime _currentDate =
+  DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
 
   List<charts.Series<MapEntry<DateTime, int>, DateTime>> _habitsGraph(){
     Map<DateTime, int> record = <DateTime, int>{
-      //Record will contain the start date the user began the course initially
+      //Record will contain the start date the user began the course
       DateTime(2018, 12, 1): 0,
+      DateTime(2018, 12, 2): 1,
+      DateTime(2018, 12, 3): 2,
+      DateTime(2018, 12, 4): 3,
+      DateTime(2018, 12, 5): 4,
+      DateTime(2018, 12, 6): 5,
+      DateTime(2018, 12, 7): 6,
+      DateTime(2018, 12, 8): 7,
+      DateTime(2018, 12, 9): 8,
     };
     HabitManager.instance.getHabits().forEach((_, val) {
       val.markedOff.forEach((date) {
         if (record.containsKey(date))
-          record.update(date, (val) => val++);
+          record.update(date, (val) => val *= 2);
         else
           record.putIfAbsent(date, () => 1);
       });
@@ -45,6 +54,16 @@ class _ProgressState extends State<ProgressWidget>{
     ];
   }
 
+  int _habitsChecked(){
+    int checked = 0;
+    HabitManager.instance.getHabits().forEach((_, val) {
+      if (val.markedOff.contains(_currentDate)){
+        checked += 1;
+      }
+    });
+    return checked;
+  }
+
   Widget build(context){
     return Column(
         children: <Widget>[
@@ -52,11 +71,25 @@ class _ProgressState extends State<ProgressWidget>{
               constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height/2),
               child: charts.TimeSeriesChart(
                   _habitsGraph(),
-                  domainAxis: new charts.EndPointsTimeAxisSpec()
+                  defaultRenderer: charts.LineRendererConfig(includePoints: true),
+                  domainAxis: charts.DateTimeAxisSpec(
+                    tickFormatterSpec: charts.AutoDateTimeTickFormatterSpec(
+                      day: charts.TimeFormatterSpec(
+                        format: 'd/MM', transitionFormat: 'dd/MM/yyyy'
+                      )
+                    ),
+                  )
               )
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Habits tracked today: ${_habitsChecked()}/"
+                  "${HabitManager.instance.getHabits().length}",
+                  style: Theme.of(context).textTheme.title)
+            ],
+          )
         ],
-
     );
   }
 }
