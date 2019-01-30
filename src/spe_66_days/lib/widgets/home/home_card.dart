ad7d5b610cef58c2e3772e62b4c17df086695360
Unknown <jacob_chalkie_@hotmail.co.uk>
2019-01-30 13:13:14 +0000

    Pedantic double values (again)

--- src/spe_66_days/lib/widgets/home/home_card.dart
+++ src/spe_66_days/lib/widgets/home/home_card.dart
@@ -5,14 +5,14 @@ class HomeCard extends Card {
     Key key,
     String title,
     Widget child,
-  ) : super(key: key, elevation:5.0, child: Container(padding: EdgeInsets.all(5), child: Column(
+  ) : super(key: key, elevation:5.0, child: Container(padding: EdgeInsets.all(5.0), child: Column(
 
     children: <Widget>[
       Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
       Text(title, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))]),
-      Container(child:child, constraints: BoxConstraints(maxHeight: 275))
+      Container(child:child, constraints: BoxConstraints(maxHeight: 275.0))
     ],
   ))) {
 
