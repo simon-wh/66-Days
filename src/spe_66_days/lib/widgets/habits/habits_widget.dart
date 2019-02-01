355efb771063f1031230c76a4a455ada06e3a848
Simon <simon.whyte.lb@gmail.com>
2019-02-01 11:19:43 +0000

    Fix Habits on home page taking up max possible card height

--- src/spe_66_days/lib/widgets/habits/habits_widget.dart
+++ src/spe_66_days/lib/widgets/habits/habits_widget.dart
@@ -33,6 +33,51 @@ class _HabitsState extends State<HabitsWidget> {
 
 
   Widget build(BuildContext context) {
+    Column view = new Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new ListView.builder(
+            shrinkWrap: true,
+            itemCount: HabitManager.instance.getHabits().length,
+            itemBuilder: (BuildContext context, int index) {
+              MapEntry<String, CoreHabit> entry = HabitManager.instance.getHabits().entries.toList()[index];
+              CoreHabit _habit = entry.value;
+              var content = HabitListWidget(_habit, editable: !this.widget.compact, displayMode: this.widget.compact ? mode.Minimal : mode.Standard);
+
+              return entry.key.startsWith(HabitManager.customHabitPrefix) ? Dismissible(
+                  direction: DismissDirection.startToEnd,
+                  // Each Dismissible must contain a Key. Keys allow Flutter to
+                  // uniquely identify Widgets.
+                  key: Key(entry.key),
+                  // We also need to provide a function that will tell our app
+                  // what to do after an item has been swiped away.
+                  background: Container(
+                    color: Colors.red,
+                    child: Icon(Icons.delete),
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.all(5.0),
+                  ),
+                  onDismissed: (direction) {
+                    // Remove the item from our data source.
+                    setState(() {
+                      HabitManager.instance.removeHabit(entry.key);
+                      HabitManager.instance.save();
+                    });
+
+                    // Show a snack bar! This snack bar could also contain "Undo" actions.
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text("Custom Habit \"${_habit.title}\" removed")));
+                  },
+                  child: content
+              ) : content;
+            } // Item Builder
+        )
+      ],
+    );
+
+    if (this.widget.compact)
+      return view;
+    else {
     return Scaffold(
       floatingActionButton: !this.widget.compact? FloatingActionButton.extended(
           onPressed: () {
@@ -45,49 +90,8 @@ class _HabitsState extends State<HabitsWidget> {
           label: const Text('Add Habit')) : null,
       body:  Container(
       padding: const EdgeInsets.only(top: 10.0),
-      child: new Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          new Flexible(
-              child: new ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: HabitManager.instance.getHabits().length,
-                  itemBuilder: (BuildContext context, int index) {
-                    MapEntry<String, CoreHabit> entry = HabitManager.instance.getHabits().entries.toList()[index];
-                    CoreHabit _habit = entry.value;
-                    var content = HabitListWidget(_habit, editable: !this.widget.compact, displayMode: this.widget.compact ? mode.Minimal : mode.Standard);
-
-                    return entry.key.startsWith(HabitManager.customHabitPrefix) ? Dismissible(
-                        direction: DismissDirection.startToEnd,
-                        // Each Dismissible must contain a Key. Keys allow Flutter to
-                        // uniquely identify Widgets.
-                        key: Key(entry.key),
-                        // We also need to provide a function that will tell our app
-                        // what to do after an item has been swiped away.
-                        background: Container(
-                          color: Colors.red,
-                          child: Icon(Icons.delete),
-                          alignment: Alignment.centerLeft,
-                          padding: EdgeInsets.all(5.0),
-                        ),
-                        onDismissed: (direction) {
-                          // Remove the item from our data source.
-                          setState(() {
-                            HabitManager.instance.removeHabit(entry.key);
-                            HabitManager.instance.save();
-                          });
-
-                          // Show a snack bar! This snack bar could also contain "Undo" actions.
-                          Scaffold.of(context).showSnackBar(
-                              SnackBar(content: Text("Custom Habit \"${_habit.title}\" removed")));
-                        },
-                        child: content
-                    ) : content;
-                  } // Item Builder
-              )
-          ),
-        ],
-      ),
+      child: view,
     ));
+  }
   } // Build
 } // _HabitsState
