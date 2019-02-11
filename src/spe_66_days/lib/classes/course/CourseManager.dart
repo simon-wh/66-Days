4e87f737bbe336abafbd6cebdb2186a298b2c5d7
Unknown <jacob_chalkie_@hotmail.co.uk>
2019-02-11 11:36:14 +0000

    Capitalized course content

--- src/spe_66_days/lib/classes/course/CourseManager.dart
+++ src/spe_66_days/lib/classes/course/CourseManager.dart
@@ -14,21 +14,21 @@ class CourseManager extends SettingsBase<CourseSettings> {
     CourseEntry("Week Two\nEat Slowly and Savour Your Food", <CourseEntryItem>[
       CourseEntryText("Select an Experiment to Apply."),
       CourseEntryChange("Experiments", "eat_slowly", "experimentTitle", <String>[
-        "put down your cutlery after each mouthful",
-        "eat with your non-dominant hand",
-        "set a chew goal per mouthful",
-        "halve every mouthful before you eat it",
-        "drink water between every mouthful",
-        "use chopsticks"
+        "Put down your cutlery after each mouthful",
+        "Eat with your non-dominant hand",
+        "Set a chew goal per mouthful",
+        "Halve every mouthful before you eat it",
+        "Drink water between every mouthful",
+        "Use chopsticks"
       ]),
       CourseEntryChange("Environment Design", "eat_slowly", "environmentDesign", <String>[
-        "eat your meals at a table. Never eat at your desk",
-        "eat in a calm environment",
-        "give yourself time to eat",
-        "don’t eat in front of a screen",
-        "eat with other people when possible",
-        "don't eat out of a packet - plate up everything, even if it's just a snack",
-        "try and relish what you're eating - be grateful and enjoy it!"
+        "Eat your meals at a table. Never eat at your desk",
+        "Eat in a calm environment",
+        "Give yourself time to eat",
+        "Don’t eat in front of a screen",
+        "Eat with other people when possible",
+        "Don't eat out of a packet - plate up everything, even if it's just a snack",
+        "Try and relish what you're eating - be grateful and enjoy it!"
       ])
     ])
   ];
