3dbbf0627c866b9bc2f07bbccfd0d5f694b4f4c7
James Hancock <jhancock532@gmail.com>
2019-03-13 11:56:48 +0000

    Added Dashboard and Whitelist Test Files

---
+++ src/test/java/tests/DashboardTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import javax.transaction.Transactional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import packages.Application;
+
+@Transactional //Rolls back changes to the database after testing.
+@SpringBootTest(classes=Application.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DashboardTests {
+    
+    @Test
+    public void assertAlive(){
+        assert(true);
+    }
+    
+}
