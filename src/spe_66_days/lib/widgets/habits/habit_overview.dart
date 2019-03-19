c2428c89d03ad457d2072c43a3a1e3ca94e872d2
Simon <simon.whyte.lb@gmail.com>
2019-03-19 20:31:49 +0000

    Added ability to change Habit checking for older days in Overview

--- src/spe_66_days/lib/widgets/habits/habit_overview.dart
+++ src/spe_66_days/lib/widgets/habits/habit_overview.dart
@@ -5,6 +5,7 @@ import '../progress/progress_widget.dart';
 import 'package:spe_66_days/classes/Global.dart';
 import 'package:spe_66_days/widgets/habits/edit_habit_widget.dart';
 import 'package:spe_66_days/widgets/progress/streaks_chart.dart';
+import 'package:intl/intl.dart';
 
 class HabitOverview extends StatefulWidget {
   final String habitKey;
@@ -20,6 +21,8 @@ class HabitOverviewState extends State<HabitOverview> {
 
   Widget build(context) {
     var habit = Global.habitManager.getHabit(this.widget.habitKey);
+    var formatter = new DateFormat('dd MMM');
+    var startDate = habit.startDate;
     return new Scaffold(
         appBar:
             AppBar(title: Text(habit.title + " Overview"), actions: <Widget>[
@@ -47,6 +50,17 @@ class HabitOverviewState extends State<HabitOverview> {
               title: Text(habit.title),
               subtitle: Text(habit.experimentTitle + (habit.environmentDesign == null ? "" : '\n' + habit.environmentDesign)),
               isThreeLine: habit.environmentDesign != null
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
+              height: 75,
+              child: ListView.builder(scrollDirection: Axis.horizontal,itemCount: Global.currentDate.difference(startDate).inDays,  itemBuilder: (context, i){
+                var date = Global.currentDate.add(Duration(days: -i));
+                return Card(child: Stack(children: <Widget>[
+                  Align(alignment: Alignment.topCenter,  child: Text(formatter.format(date))),
+                    Align(alignment: Alignment.center,  child: Checkbox(value: habit.markedOff.contains(date), onChanged: (checked) => setState(()=>Global.habitManager.setCheckHabit(this.widget.habitKey, checked, date:date))))
+                ]));
+              })
           ),
           Container(
               constraints: BoxConstraints(maxHeight: 300.0),
